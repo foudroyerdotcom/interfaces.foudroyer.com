1358ae153223066c30824c4877a78285d7406e10
Marques Kevin <marques.developpement@gmail.com>
2024-05-07 10:46:05 +0200

    feat: user agent

--- src/entities/WebsiteEntity.ts
+++ src/entities/WebsiteEntity.ts
@@ -1,5 +1,8 @@
 import { IndexationGoogleCloudApiKeyEntity } from "./IndexationEntity";
 
+export const USER_AGENT =
+  "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Foudroyer";
+
 export class WebsiteEntity {
   id: string;
   search_console_domain: string | null;
