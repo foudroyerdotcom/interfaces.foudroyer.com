fd805125137b57118644e8c197fed01e7552fe8a
Marques Kevin <marques.developpement@gmail.com>
2024-06-20 20:25:03 +0200

    feat: ajout de LogsEntity

---
+++ src/entities/LogsEntity.ts
@@ -0,0 +1,38 @@
+type LogsBaseEntity = {
+  id: string
+  fk_website_id: string
+
+  title: string
+  description: string
+
+  tags: string[]
+
+  log_date: Date
+
+  created_at: Date
+  updated_at: Date
+
+  clicks: number | null
+  impressions: number | null
+}
+
+type LogQueryEntity = {
+  type: "query"
+  query: string
+  page: null
+}
+
+type LogGlobalEntity = {
+  type: "global"
+  query: null
+  page: null
+}
+
+type LogPageEntity = {
+  type: "page"
+  query: null
+  page: string
+}
+
+export type LogsEntity = LogsBaseEntity &
+  (LogGlobalEntity | LogPageEntity | LogQueryEntity)
