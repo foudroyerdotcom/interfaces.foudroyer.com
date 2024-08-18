a6081ef0db49ea0cba86fc6b1f3a24af15b610f1
Marques Kevin <marques.developpement@gmail.com>
2024-08-18 12:44:47 +0200

    feat(WebsiteEntity): sitemap_region_proxy

--- src/entities/WebsiteEntity.ts
+++ src/entities/WebsiteEntity.ts
@@ -3,6 +3,11 @@ import { IndexationGoogleCloudApiKeyEntity } from "./IndexationEntity";
 export const USER_AGENT =
   "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Foudroyer";
 
+export enum SitemapRegionProxies {
+  fr = "fr",
+  us = "us",
+}
+
 export class WebsiteEntity {
   id: string;
   search_console_domain: string | null;
@@ -23,4 +28,5 @@ export class WebsiteEntity {
   google_api_keys: IndexationGoogleCloudApiKeyEntity[];
   does_google_api_keys_have_errors: boolean;
   does_sitemaps_have_errors: boolean;
+  sitemap_region_proxy: SitemapRegionProxies;
 }
