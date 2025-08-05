24f9ced374a93a4f48edff596cfb49cbade34a4f
Kevin Marques <marques.developpement@gmail.com>
2025-08-05 09:32:45 +0200

    fix: add dto in main

--- src/dto/keywords_create_keywords_dto.ts
+++ src/dto/keywords_create_keywords_dto.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
-export const keywords_create_keyword = {
+export const keywords_create_keywords_dto = {
   url: "/keywords/create_keywords",
   request: z.object({
     website_id: z.string(),
